84cae8425364d7ab8b5470cb78dc4d288f4074b6
MedNadirRhazi <mrhaz005@uottawa.ca>
2024-06-20 14:31:26 +0100

    all set

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for, request, send_file
 import random, time
+import pandas as pd
+from io import BytesIO
 
 app = Flask(__name__)
 app.secret_key = 'une_cle_secrete_tres_complexe'
@@ -27,6 +29,16 @@ def get_trials():
         correct_key = 'A' if position == 'left' else 'L'
         trials.append({'position': position, 'correct_key': correct_key})
     return jsonify(trials=trials)
+
+@app.route('/download-excel', methods=['POST'])
+def download_excel():
+    data = request.json['results']
+    df = pd.DataFrame(data)
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False)
+    output.seek(0)
+    return send_file(output, as_attachment=True, download_name="results.xlsx")
 
 if __name__ == '__main__':
     app.run(debug=True)
